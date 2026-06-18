c59402eec055186135b064091961d3111af6cfc8
akshah <memorialacademyakshah@gmail.com>
2026-06-18 03:14:07 -0700

    NECESSARY UPDATE

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -186,7 +186,7 @@ export default function Footer() {
                   text-[#08104d]
                 "
               >
-                AdvanceIndiaIT
+                ASMA
               </h2>
 
               <p
